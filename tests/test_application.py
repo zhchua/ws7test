87d5f83e95feeb2f6069389275d93fcfa1bd27b7
zhchua <42735695+zhchua@users.noreply.github.com>
2019-08-16 11:09:14 +0800

    Fix test assert
    should make it run

--- tests/test_application.py
+++ tests/test_application.py
@@ -11,4 +11,5 @@ def test_response(client):
     response_body = json.loads(result.get_data())
     assert result.status_code == 200
     assert result.headers['Content-Type'] == 'application/json'
-    assert response_body['Output'] == 'Hello World'
+    assert response_body['Output'] == 'Hello World from Codestar'
+    #return Response(json.dumps({'Output': 'Hello World from Codestar'}), mimetype='application/json', status=200)
